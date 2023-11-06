5cedbe6620faad7ca09b8b0a46a16a2c7b54cda4
fairuuz16 <firuz.firaz@gmail.com>
2023-11-06 08:51:57 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+const url = 'https://pokeapi.co/api/v2/pokemon?limit=20&offset=0';
+
+async function getData() {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    const data = await response.json();
+    display(data.results);
+  } catch (error) {
+    console.error('Error:', error);
+  }
+}
+
+function display(pokemonArray) {
+  for (let i = 0; i < 20; i++) {
+    const nama = document.getElementById(`nama${i + 1}`);
+    nama.textContent = pokemonArray[i].name;
+    nama.classList.add("card-name");
+  }
+}
+
+getData();
