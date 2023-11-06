e2ef43104dfc128fc00c5b79203556bdaa7c48e3
fairuuz16 <firuz.firaz@gmail.com>
2023-11-06 10:32:45 +0700

    revisi

--- script.js
+++ script.js
@@ -1,24 +1,26 @@
 const url = 'https://pokeapi.co/api/v2/pokemon?limit=20&offset=0';
 
 async function getData() {
-  try {
-    const response = await fetch(url);
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      const data = await response.json();
+      display(data.results);
+    } catch (error) {
+      console.error('Error:', error);
     }
-    const data = await response.json();
-    display(data.results);
-  } catch (error) {
-    console.error('Error:', error);
   }
-}
+  
 
 function display(pokemonArray) {
   for (let i = 0; i < 20; i++) {
     const nama = document.getElementById(`nama${i + 1}`);
-    nama.textContent = pokemonArray[i].name;
+    nama.textContent = pokemonArray[i].name.charAt(0).toUpperCase() + pokemonArray[i].name.slice(1);
     nama.classList.add("card-name");
   }
 }
+
 
 getData();
